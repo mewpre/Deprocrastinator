6af743158fa6aba2d3425b987bb54d16b8685b04
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-12 13:53:40 -0800

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Tewodros Wondimu on 1/12/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
